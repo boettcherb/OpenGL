421e2fbbb099015e3c938ea2edf7626d069c2d91
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-23 02:16:29 -0500

    Changed the shaders to display a mixture of 2 textures and some color

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -3,8 +3,14 @@
 #version 330 core
 out vec4 color;
 
-in vec3 v_color;
+in vec4 v_color;
+in vec2 v_texCoord;
+
+uniform sampler2D u_texture_face;
+uniform sampler2D u_texture_shelf;
 
 void main() {
-    color = vec4(v_color, 1.0f);
+    vec4 face = texture(u_texture_face, v_texCoord);
+    vec4 shelf = texture(u_texture_shelf, v_texCoord);
+    color = mix(shelf, face, 0.2f) * v_color;
 }
--- res/shaders/vertex.glsl
+++ res/shaders/vertex.glsl
@@ -1,12 +1,15 @@
 // VERTEX
 
 #version 330 core
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_color;
+layout(location = 2) in vec2 a_texCoord;
 
-out vec3 v_color;
+out vec4 v_color;
+out vec2 v_texCoord;
 
 void main() {
-    gl_Position = vec4(position, 1.0f);
-    v_color = color;
+    gl_Position = vec4(a_position, 1.0f);
+    v_color = vec4(a_color, 1.0f);
+    v_texCoord = a_texCoord;
 }
