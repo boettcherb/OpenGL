0f470687e7e276f25e445653d77f80a338bb2e5c
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-23 19:55:27 -0500

    Changed the vertex shader to take in a transformation matrix

--- res/shaders/vertex.glsl
+++ res/shaders/vertex.glsl
@@ -8,8 +8,10 @@ layout(location = 2) in vec2 a_texCoord;
 out vec4 v_color;
 out vec2 v_texCoord;
 
+uniform mat4 u_trans;
+
 void main() {
-    gl_Position = vec4(a_position, 1.0f);
+    gl_Position = u_trans * vec4(a_position, 1.0f);
     v_color = vec4(a_color, 1.0f);
     v_texCoord = a_texCoord;
 }
